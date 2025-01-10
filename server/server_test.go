b621a0bba3ba476752431e401b379aa2036d9df7
adeeshajayasinghe <jayasingheai.20@uom.lk>
2025-01-10 11:53:43 +0530

    Tested server with TLS authentication

--- server/server_test.go
+++ server/server_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"go-authorization/config"
 	"go-authorization/log"
 	spec "go-authorization/spec"
 	"net"
@@ -9,30 +10,47 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func TestServer(t *testing.T) {
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	ctx := context.Background()
 	require.NoError(t, err)
 
-	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
-	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CAFile: config.CAFile,
+	})
 	require.NoError(t, err)
+
+	clientCred := credentials.NewTLS(clientTLSConfig)
+
+	cc, err := grpc.Dial(l.Addr().String(), grpc.WithTransportCredentials(clientCred))
+	require.NoError(t, err)
+
+	client := spec.NewLogClient(cc)
+
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile: config.ServerCertFile,
+		KeyFile: config.ServerKeyFile,
+		CAFile: config.CAFile,
+		ServerAddress: l.Addr().String(),
+	})
+	require.NoError(t, err)
+
+	serverCreds := credentials.NewTLS(serverTLSConfig)
 
 	clog := log.NewLog()
 
 	cfg := &Config{
 		CommitLog: clog,
 	}
-	server, err := NewGRPCServer(cfg)
+	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
 	require.NoError(t, err)
 
 	go func ()  {
 		server.Serve(l)	
 	}()
-
-	client := spec.NewLogClient(cc)
 	
 	want := &spec.Record{
 		Value: []byte("hello world"),
